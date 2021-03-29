6aac86711f54483db478ec2a9052723a26d6d7ec
MuratKunanbaev <kunanbaevmurat5@gmail.com>
2021-03-29 18:28:34 +0500

    6.0.2. Заполнение массива. [#501135]

--- src/array/ArrayDefinition.java
+++ src/array/ArrayDefinition.java
@@ -1,5 +1,7 @@
 package array;
 
+import javax.sound.midi.Soundbank;
+
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
@@ -10,5 +12,15 @@ public class ArrayDefinition {
 
         float[] prices = new float[40];
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Ivanov Ivan";
+        names[1] = "Petrov Petr";
+        names[2] = "Mihailov Mihail";
+        names[3] = "Yakovlev Yakov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
