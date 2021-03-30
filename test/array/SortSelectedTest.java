f897ecc2e706029ccd73f1ff2b2bf6cbb84d2b9e
MuratKunanbaev <kunanbaevmurat5@gmail.com>
2021-03-30 22:31:28 +0500

    6.5.3. Сортировка выборкой. [#501129]

---
+++ test/array/SortSelectedTest.java
@@ -0,0 +1,27 @@
+package array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SortSelectedTest {
+
+    @Test
+    public void whenSortBy5Numbers() {
+        int[] data = new int[] {3, 4, 1, 2, 5};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3, 4, 5};
+        Assert.assertArrayEquals(expected, result);
+
+    }
+
+    @Test
+    public void whenSortBy3Numbers() {
+        int[] data = new int[] {13, 20, 1};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 13, 20};
+        Assert.assertArrayEquals(expected, result);
+
+    }
+}
